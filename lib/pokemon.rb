1821229b0dfd42bb86732e0e20bc19be39026752
ronald vilorio <soccer23415@gmail.com>
2018-01-04 05:53:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id,:name,:type,:db
+  @database_connection = SQLite3::Database.new('db/pokemon.db')
+
+
+  def initialize(attributes)
+    attributes.each{|key,value| self.send("#{key}=",value)}
+  end
+
+  def self.save(name,type,db)
+    database_connection.execute("INSERT INTO pokemon")
+    
+
+  end
+
 end
