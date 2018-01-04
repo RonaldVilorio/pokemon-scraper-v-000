2b68b301faceb2985883dafb3ea6cad341764582
ronald vilorio <soccer23415@gmail.com>
2018-01-04 07:32:15 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
     # binding.pry
   end
   def alter_hp(hp,db)
-    db.execute("UPDATE TABLE pokemon SET hp =  ")
+    db.execute("UPDATE TABLE pokemon SET hp = (?)",hp)
     binding.pry
 
   end
